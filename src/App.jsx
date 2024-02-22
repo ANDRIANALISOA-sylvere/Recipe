8a437387929f55e622e5e1e4ec8a1f44cefb90e8
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-02-22 12:55:55 +0300

    styling the card

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         setSearchRecipes(data.meals);
         setInitialRecipe(null);
       })
+      .catch(err=> console.error(err))
     }
   },[input])
 
@@ -34,25 +35,28 @@ function App() {
   return (
     <div>
       <div className="m-5">
-    
-        <div className="w-72">
-            <input className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 mb-2 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Chercher une recette" placeholder="Chercher une recette..." value={input} onChange={(e)=> handleChange(e)}></input>
+            <div className="flex justify-center">
+                <input className="w-72 focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none text-sm leading-6 mb-2 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Chercher une recette" placeholder="Chercher une recette..." value={input} onChange={(e)=> handleChange(e)}></input>
+            </div>
+        <div className="grid grid-cols-3 w-100 gap-2">
             {initialRecipe && (
-              <div className="initial-recipe shadow mt-5 bg-slate-50">
-                <img src={initialRecipe.strMealThumb} alt={initialRecipe.strMeal} />
-                <h1 className="font-bold text-center text-xl pb-3 mt-2">{initialRecipe.strMeal}</h1>
+              <div className="flex justify-center">
+                <div className="w-72 shadow mt-5 bg-slate-50">
+                  <img src={initialRecipe.strMealThumb} alt={initialRecipe.strMeal} />
+                  <h1 className="font-bold text-center text-xl pb-3 mt-2">{initialRecipe.strMeal}</h1>
+                </div>
               </div>
             )}
-            {searchRecipes ? (
-              searchRecipes.map((recipe) => (
-                <div key={recipe.idMeal} className="recipe-item shadow mt-5 bg-slate-50">
-                  <img src={recipe.strMealThumb} alt={recipe.strMeal} />
-                  <h1 className="font-bold text-center text-xl pb-3 mt-2">{recipe.strMeal}</h1>
-                </div>
-            ))
-          ) : (
-            <p>Loading...</p>
-          )}
+                {searchRecipes ? (
+                    searchRecipes.map((recipe) => (
+                      <div key={recipe.idMeal} className="w-56 shadow mt-5 bg-slate-50">
+                        <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+                        <h1 className="font-bold text-center text-xl pb-3 mt-2">{recipe.strMeal}</h1>
+                        </div>
+    ))          ) : (
+                <p>Loading...</p>
+              )}
+            
         </div>
         
     </div>
